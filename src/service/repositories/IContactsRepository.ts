40533ded4af2abfc00d182062049145f673a91a4
Marcus <marcus10121@hotmail.com>
2023-01-02 22:11:49 -0300

    :sparkles: feat: Atualiza página de contátos

--- src/service/repositories/IContactsRepository.ts
+++ src/service/repositories/IContactsRepository.ts
@@ -2,13 +2,11 @@ export interface contacts  {
   contactName: string;
   contactImage: string;
   contactUrl: string;
-  contactCardColor: string;
 }
 
 /** Exemplo: gmail =  {
  * contactName: 'GMail',
  * contactImage: 'public/gmailIcon',
  * contactUrl: 'markosf.dev@gmail.com'
- * contactCardColor: 'red'
  * } */
 
